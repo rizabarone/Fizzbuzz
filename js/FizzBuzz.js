b89027b36237d97c6bfc62d4ed1b68c8e64cefaa
Riza Barone <ysa_e@hotmail.com>
2015-11-15 17:46:41 -0500

    committing fizzbuzz challenge

---
+++ js/FizzBuzz.js
@@ -0,0 +1,19 @@
+
+   (function executeThis() {
+
+    function FizzBuzz(numCount) {
+        if (numCount % 15 == 0) {
+            document.write("FizzBuzz ");
+        } else if (numCount % 3 == 0) {
+            document.write("Fizz ");
+        } else if (numCount % 5 == 0) {
+            document.write("Buzz ");
+        } else {
+            document.write(numCount + ' ');
+        }
+    }
+
+    for (var numCount = 1; numCount < 100; numCount++) {
+        FizzBuzz(numCount);
+    }
+}());
